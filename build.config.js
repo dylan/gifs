a78f5260bdf5f1c4bb74bd338a955c580c66e44c
Dylan Wreggelsworth <dylan@bvr.group>
2018-12-20 11:53:32 -0500

    lib updates, start using terser

--- build.config.js
+++ build.config.js
@@ -1,14 +1,12 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
+import { terser } from "rollup-plugin-terser"
 
 export default {
   plugins: [
+    terser(),
     babel({
-      babelrc: false,
-      presets: ["es2015-rollup"],
-      plugins: [
-        ["transform-react-jsx", { pragma: "h" }]
-      ]
+      plugins: [["transform-react-jsx", { pragma: "h" }]]
     }),
     resolve({
       jsnext: true
--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,12 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
+import { terser } from "rollup-plugin-terser"
 
 export default {
   plugins: [
+    terser(),
     babel({
-      babelrc: false,
-      presets: ["es2015-rollup"],
       plugins: [
         ["transform-react-jsx", { pragma: "h" }]
       ]
